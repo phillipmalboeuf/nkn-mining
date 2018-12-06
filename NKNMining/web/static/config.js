45fc9d67a690f96093b1f395bbb4b9a68a2682e1
AKACoder <akacoder@outlook.com>
2018-12-06 16:13:10 +0800

    remove the debug server ip
    Signed-off-by: AKACoder <akacoder@outlook.com>

--- NKNMining/web/static/config.js
+++ NKNMining/web/static/config.js
@@ -1,5 +1,5 @@
 var nknShellWebConfig = {
-  baseURI:"http://127.0.0.1:8181/api/v1",
+  baseURI:"/api/v1",
   nknDataCenterURL: "http://35.202.14.190:7890/api/v1",
   nknNode: "http://35.197.90.102:30003"
 }
