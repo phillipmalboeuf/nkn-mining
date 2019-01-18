ca16c52a7a8da9b722f438f3d4c142642fa1ecfd
TDCQZD <ZDTDCQ@163.com>
2019-01-18 17:55:52 +0800

    update struct field

--- NKNMining/src/NKNMining/network/chainDataTypes/rpcApiResponse/base.go
+++ NKNMining/src/NKNMining/network/chainDataTypes/rpcApiResponse/base.go
@@ -1,9 +1,8 @@
 package rpcApiResponse
 
 type Base struct {
-	Id  string `json:"Id"`
-	Jsonrpc    string `json:"Jsonpc"`
+	Id      string `json:"id"`
+	Jsonrpc string `json:"jsonrpc"`
 }
 
 type NullData struct{}
-
